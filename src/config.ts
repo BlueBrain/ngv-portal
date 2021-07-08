2ddf2f3719a3d6c05eb9b170548585e399bd23bd
Pavlo Getta <pavlo.getta@epfl.ch>
2021-07-08 22:50:40 +0200

    Add feedback component

--- src/config.ts
+++ src/config.ts
@@ -10,4 +10,10 @@ export const accentColors: { [key: string]: string } = {
   orange: '#ed8048',
 };
 
+export const isServer = typeof window === 'undefined';
+export const isProduction = process.env.NODE_ENV === 'production';
+
+export const deploymentUrl = 'https://bbp.epfl.ch';
 export const baseUrl = '/ngv-portal';
+
+export const feedbackUrl = process.env.NEXT_PUBLIC_FEEDBACK_URL;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import Head from 'next/head';
 
 import MainLayout from '../layouts/MainLayout';
+import Feedback from '../components/Feedback';
 
 import '../styles/globals.scss';
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap" />
+      </Head>
+
+      <MainLayout>
+        <Component {...pageProps} />
+
+        <Feedback />
+      </MainLayout>
+    </>
   );
 }
 
