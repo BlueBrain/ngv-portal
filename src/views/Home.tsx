5ea78509a6f6e2680267e4c4a8e92585ecd4f436
Pavlo Getta <pavlo.getta@epfl.ch>
2021-08-23 11:42:13 +0200

    Update text and style for the Explore section

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -142,7 +142,7 @@ const Home: React.FC = () => (
                 severely restricted by the lack of detailed cytoarchitecture (cellular structure
                 of a biological tissue). To address this problem, we used data from multiple
                 sources to create a data-driven digital reconstruction of the NGV at micrometer
-                anatomical resolution. We reconstructed 0.2 mm3 of rat somatosensory cortical
+                anatomical resolution. We reconstructed 0.2 mm<sup>3</sup> of rat somatosensory cortical
                 tissue with approximately 16,000 morphologically detailed neurons, its
                 microvasculature, and approximately 2,500 morphologically detailed protoplasmic
                 astrocytes. In order to maintain the local connectivity, the microcircuit was
@@ -182,11 +182,15 @@ const Home: React.FC = () => (
                 of the brain energy metabolism has been recognized for decades, a complete
                 understanding of the metabolic processes supporting and constraining the brain
                 function remains elusive.
+              </p>
 
+              <p>
                 We built a biologically detailed bottom-up reconstruction and simulation of the
                 NGV Unit and the rat neocortical microcircuitry energy metabolism driven by its
                 electrophysiological activity and constrained by bidirectional feedback.
+              </p>
 
+              <p>
                 A multitude of reactions define energy metabolism at the molecular level. The
                 metabolism simulation features the most important biochemical pathways that
                 provide the brain with the energy for complex neuronal activity. It includes
@@ -199,7 +203,9 @@ const Home: React.FC = () => (
                 located in the cytosol and mitochondria. The model is validated against
                 published data, including the BOLD signal, OGI, and concentrations of
                 metabolites.
+              </p>
 
+              <p>
                 We simulated the neocortical microcircuitry coupled to the metabolism. To
                 achieve this goal, we provided every neuron in the neocortical column with type-
                 and layer-specific metabolism model. Bidirectional communication between the
@@ -229,17 +235,26 @@ const Home: React.FC = () => (
                 body’s energy. Furthermore, there is a limited capacity to store energy in the
                 brain. There is therefore a highly oxygen/glucose dependency in the brain supply
                 from the bloodstream.
+              </p>
+
+              <p>
                 Blood flow is regulated by neurons and  astrocytes.
                 Astrocytes, a type of cell in the central nervous system, play an important role
                 in metabolism. Each astrocyte has an endfoot which connects to the blood
                 vessels. Increased neuronal activity elevates calcium levels in astrocytes,
                 which in turn releases vasoactive substances at the endfeet, causing changes in
                 the diameter of the associated blood vessel.
+              </p>
+
+              <p>
                 Blood flow through the blood vessel (vasculature) network changes in response to
                 altered diameters, meaning that delivery of nutrients, lactate, glucose, and
                 oxygen to support neuronal metabolism is in turn changed.
                 We made the whole NGV unit working together by integrating astrocytes to
                 vasculature in a simulation.
+              </p>
+
+              <p>
                 Release of glutamate at synapses can cause local elevation of calcium within
                 astrocytic processes. We have adapted a model of calcium from Savchenko 2019 to
                 run within the neuronal microcircuit. Resulting elevation of calcium levels
