795e6a49bd684ba777ec71e28a024a8e8d92c075
Marta Pałczyńska <marta.palczynska@collibra.com>
2021-09-09 09:51:28 +0200

    Navigation on hover

--- src/layouts/NavDesktop/index.tsx
+++ src/layouts/NavDesktop/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
@@ -8,13 +8,16 @@ import { HomeNav, SecondaryNav } from '../Navigation';
 
 const classPrefix = 'nav-desktop__';
 
+const delay = 250;
+
 type NavButtonProps = {
   path: string;
   name: string;
   notifications?: number;
   home?: boolean;
   highlight?: boolean;
-  onClick?: () => void;
+  toggleClick?: () => void;
+  onMouseLeave?: () => void;
 };
 
 const NavButton: React.FC<NavButtonProps> = ({
@@ -23,20 +26,29 @@ const NavButton: React.FC<NavButtonProps> = ({
   notifications,
   home,
   highlight,
-  onClick,
+  onMouseLeave
 }) => {
+  const router = useRouter();
+  const isEqual = router.pathname === path;
+
+  const button = (
+    <Button
+      width={highlight || home ? 140 : undefined}
+      discrete={!highlight && !home}
+      notifications={notifications}
+      onMouseLeave={onMouseLeave}
+      uppercase
+    >
+      {name}
+    </Button>
+  )
+  if(isEqual) {
+    return button;
+  }
   return (
     <Link href={path}>
       <a>
-        <Button
-          width={highlight || home ? 140 : undefined}
-          discrete={!highlight && !home}
-          onClick={onClick}
-          notifications={notifications}
-          uppercase
-        >
-          {name}
-        </Button>
+        {button}
       </a>
     </Link>
   );
@@ -44,31 +56,63 @@ const NavButton: React.FC<NavButtonProps> = ({
 
 const NavDesktop = () => {
   const router = useRouter();
+  const [mouseOverButton, setMouseOverButton] = useState(false);
+  const [mouseOverMenu, setMouseOverMenu]=useState(false);
 
-  const [secondaryNav, setSecondaryNav] = React.useState(false);
-  React.useEffect(() => setSecondaryNav(false), [router]);
+  const enterButton = () => {
+    setMouseOverButton(true)
+  }
+
+  const leaveButton = () => {
+    setTimeout(() => {
+      setMouseOverButton(false)
+    }, delay);
+  }
+
+  const enterMenu = () => {
+    setMouseOverMenu(true)
+  }
+
+  const leaveMenu = () => {
+     setTimeout(() => {
+       setMouseOverMenu(false);
+     }, delay);
+  }
+
+ React.useEffect(() => {
+   setMouseOverButton(false);
+   setMouseOverMenu(false);
+ }, [router]);
+
+ const isOpen = mouseOverButton || mouseOverMenu;
 
   return (
     <ul className={`${classPrefix}basis`}>
-      <li style={{ position: 'relative' }}>
-        {secondaryNav ? (
-          <NavButton path="/" name="Home" home />
-        ) : (
+      <li 
+        style={{ position: 'relative' }} 
+      >
+        {isOpen ? (
+          <>
+            <NavButton path="/" name="Home" home onMouseLeave={leaveButton}/>
+            <div className="menu-container" onMouseEnter={enterMenu} onMouseLeave={leaveMenu}>
+              <div className="flyout">
+              <HomeNav />
+              <SecondaryNav />
+            </div>
+            </div>
+          </>) 
+          : (
           <Button
             width={140}
             active={router.pathname === '/'}
-            onClick={() => setSecondaryNav(true)}
+            onMouseEnter={enterButton}
+            onMouseLeave={leaveButton}
             uppercase
           >
             Home
           </Button>
         )}
-        {secondaryNav && (
-          <div className="flyout">
-            <HomeNav />
-            <SecondaryNav initActive="exp" />
-          </div>
-        )}
+
       </li>
       <li>
         <NavButton path="/glossary" name="Glossary" />
