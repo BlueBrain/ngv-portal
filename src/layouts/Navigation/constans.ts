22def11083717248010234abfbabb77fa6bc41a0
Marta Pałczyńska <marta.palczynska@collibra.com>
2021-09-09 11:42:38 +0200

    Fix constant file

--- src/layouts/Navigation/constans.ts
+++ src/layouts/Navigation/constans.ts
@@ -1,64 +1,64 @@
 import { accentColors } from '../../config';
 
 export enum Urls  {
-    ANATOMY = '/anatomy',
-    METABOLISM = '/metabolism',
-    BLOOD_FLOW = 'blood-flow',
-    EXPERIMENTAL_DATA = '/experimental-data',
-    RECONSTRUCION_DATA = '/reconstruction-data',
-    VALIDATION  = '/validations',
-    PREDICTION = '/predictions'
+  ANATOMY = '/anatomy',
+  METABOLISM = '/metabolism',
+  BLOOD_FLOW = 'blood-flow',
+  EXPERIMENTAL_DATA = '/experimental-data',
+  RECONSTRUCION_DATA = '/reconstruction-data',
+  VALIDATION  = '/validations',
+  PREDICTION = '/predictions'
 }
 
 export type NavItem = {
-    color: string;
-    id: 'exp' | 'rec' | 'dig',
-    label: string,
-    href: Urls,
-    disabled?: boolean;
-    children: Array<{
-        label: string;
-        href: Urls
-    }>
+  color: string;
+  id: 'exp' | 'rec' | 'dig',
+  label: string,
+  href: Urls,
+  disabled?: boolean;
+  children: Array<{
+    label: string;
+    href: Urls
+  }>
 }
 
 export const navItems: NavItem[] = [
-    {
-        color: accentColors.blue,
-        id: 'exp',
-        label: 'anatomy',
-        href: Urls.ANATOMY,
-        children: [
-            {label: 'Experimental Data', href: Urls.EXPERIMENTAL_DATA},
-            {label: 'Reconstruction Data', href: Urls.RECONSTRUCION_DATA},
-            {label: 'Validations', href: Urls.VALIDATION},
-            {label: 'Predictions', href: Urls.PREDICTION}
-        ]
-    },
-    {
-        color: accentColors.purple,
-        id: 'rec',
-        label: 'metabolism',
-        href: Urls.METABOLISM,
-        disabled: true,
-        children: [
-            {label: 'Experimental Data', href: Urls.EXPERIMENTAL_DATA},
-            {label: 'Reconstruction Data', href: Urls.RECONSTRUCION_DATA},
-            {label: 'Validations', href: Urls.VALIDATION},
-            {label: 'Predictions', href: Urls.PREDICTION}
-        ]
-    },
-    {
-        color: accentColors.red,
-        id: 'dig',
-        label: 'blood flow',
-        href: Urls.BLOOD_FLOW,
-        disabled: true,
-        children: [
-            {label: 'Experimental Data', href: Urls.EXPERIMENTAL_DATA},
-            {label: 'Reconstruction Data', href: Urls.RECONSTRUCION_DATA},
-            {label: 'Validations', href: Urls.VALIDATION},
-            {label: 'Predictions', href: Urls.PREDICTION}
-        ]
-    },
+  {
+    color: accentColors.blue,
+    id: 'exp',
+    label: 'anatomy',
+    href: Urls.ANATOMY,
+    children: [
+      {label: 'Experimental Data', href: Urls.EXPERIMENTAL_DATA},
+      {label: 'Reconstruction Data', href: Urls.RECONSTRUCION_DATA},
+      {label: 'Validations', href: Urls.VALIDATION},
+      {label: 'Predictions', href: Urls.PREDICTION}
+    ]
+  },
+  {
+    color: accentColors.purple,
+    id: 'rec',
+    label: 'metabolism',
+    href: Urls.METABOLISM,
+    disabled: true,
+    children: [
+      {label: 'Experimental Data', href: Urls.EXPERIMENTAL_DATA},
+      {label: 'Reconstruction Data', href: Urls.RECONSTRUCION_DATA},
+      {label: 'Validations', href: Urls.VALIDATION},
+      {label: 'Predictions', href: Urls.PREDICTION}
+    ]
+  },
+  {
+    color: accentColors.red,
+    id: 'dig',
+    label: 'blood flow',
+    href: Urls.BLOOD_FLOW,
+    disabled: true,
+    children: [
+      {label: 'Experimental Data', href: Urls.EXPERIMENTAL_DATA},
+      {label: 'Reconstruction Data', href: Urls.RECONSTRUCION_DATA},
+      {label: 'Validations', href: Urls.VALIDATION},
+      {label: 'Predictions', href: Urls.PREDICTION}
+    ]
+  },
 ]
