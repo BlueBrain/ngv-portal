1638ee4e6935504abc6f4743a2705a77f981c3ca
Pavlo Getta <pavlo.getta@epfl.ch>
2021-09-15 22:04:03 +0200

    Collapse info box component content on mobile

--- src/components/InfoBox/index.tsx
+++ src/components/InfoBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Color } from '../../types';
 
@@ -16,10 +16,20 @@ const InfoBox: React.FC<InfoBoxProps> = ({
   content,
   color = 'default',
 }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleCollapse = () => setCollapsed(!collapsed);
+
   return (
     <div className={`${classPrefix}basis ${color}`}>
       {title && <h3 className="text-white">{title}</h3>}
-      {content}
+      <div className={`content ${collapsed ? 'collapsed' : ''}`}>{content}</div>
+      <div
+        className="collapse-ctrl mt-2 hide-sm"
+        onClick={toggleCollapse}
+      >
+        {collapsed ? 'Show more' : 'Show less'}
+      </div>
     </div>
   );
 };
