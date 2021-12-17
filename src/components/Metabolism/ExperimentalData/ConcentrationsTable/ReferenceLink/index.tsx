386d89b1190a245e33d2e75446dd1d7b58154e4c
Stefano Antonel <stefanoantonel@gmail.com>
2021-12-17 09:54:35 +0100

    ReferenceLink component renders one link

--- src/components/Metabolism/ExperimentalData/ConcentrationsTable/ReferenceLink/index.tsx
+++ src/components/Metabolism/ExperimentalData/ConcentrationsTable/ReferenceLink/index.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-export default function ReferenceLink({ referenceInfo, rowId }) {
+export default function ReferenceLink({ referenceInfo }) {
 
   return (
-    <>
-      {referenceInfo.map(referenceInfo => (
-        <a
-          target="_blank"
-          className={styles.shorterLink}
-          key={rowId + referenceInfo.url}
-          href={referenceInfo.url}
-          rel="noopener noreferrer"
-        >- {referenceInfo.title}</a>
-      ))}
-    </>
+    <a
+      target="_blank"
+      className={styles.shorterLink}
+      href={referenceInfo.url}
+      rel="noopener noreferrer"
+    >- {referenceInfo.title}</a>
   );
 };
