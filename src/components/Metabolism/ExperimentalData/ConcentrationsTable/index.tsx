6f6cc776e388352637f81125df491daf5a93e63d
Stefano Antonel <stefanoantonel@gmail.com>
2021-12-23 14:30:31 +0100

    Add script for concentrations table

--- src/components/Metabolism/ExperimentalData/ConcentrationsTable/index.tsx
+++ src/components/Metabolism/ExperimentalData/ConcentrationsTable/index.tsx
@@ -85,7 +85,7 @@ export default function MetabolismExpDataView() {
         <HttpDownloadButton
           onClick={() => downloadAsJson(data, 'concentrations-factsheet.json')}
         >
-          concentrations
+          download
         </HttpDownloadButton>
       </div>
 
