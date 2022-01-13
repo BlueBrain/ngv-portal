562dfaa30ad87dc3b5064100c21309666beb6244
Stefano Antonel <stefanoantonel@gmail.com>
2022-01-13 10:18:33 +0100

    Show selected component plots

--- src/components/Metabolism/DigitalReconstruction/ModelComponents/EnzymeDetails/ItemDetails.tsx
+++ src/components/Metabolism/DigitalReconstruction/ModelComponents/EnzymeDetails/ItemDetails.tsx
@@ -4,12 +4,16 @@ import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 import styles from './styles.module.scss';
 
+import ImageViewer from '@/components/ImageViewer';
+import { staticDataBaseUrl, antBreakpoint } from '@/config';
 import { EnzymeDataItemType } from './index';
 
 
 type ItemDetailsProps = {
   enzymeData: EnzymeDataItemType;
 }
+
+const imgBase = `${staticDataBaseUrl}/metabolism/digital-reconstruction`;
 
 const ItemDetails: React.FC<ItemDetailsProps> = ({enzymeData}) => {
   return (
@@ -67,7 +71,14 @@ const ItemDetails: React.FC<ItemDetailsProps> = ({enzymeData}) => {
 
         <div className={styles.plotsContainer}>
           <h2>Plots</h2>
-          {/* { enzymeData?.figure_combo } */}
+          <ImageViewer
+            src={`${imgBase}/${enzymeData?.figure_combo}`}
+            width={1920}
+            height={1080}
+            sizes={`(min-width: ${antBreakpoint.sm}) 480px, 100vw`}
+            alt="Enzyme combo plot"
+            border
+          />
         </div>
 
       </div>
