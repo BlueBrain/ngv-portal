f119d59d6bfba3b7e4648402d8a2396f13d021bf
Stefano Antonel <stefanoantonel@gmail.com>
2022-01-28 11:02:28 +0100

    Add lorem

--- src/components/Metabolism/DigitalReconstruction/SimulationResults/index.tsx
+++ src/components/Metabolism/DigitalReconstruction/SimulationResults/index.tsx
@@ -12,21 +12,25 @@ export default function SimulationResults() {
     <>
       <h4>{textContent.simulationResults}</h4>
 
+      <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam iure doloremque, asperiores quasi, veniam fugiat, corrupti cupiditate similique ex obcaecati incidunt quos exercitationem architecto dolore rem vero at aspernatur!</p>
       <ImageViewer
         src={`${imageBasePath}/figure_placeholder_sim_synaptic_metabolism.png`}
         alt="Simulation synaptic metabolism"
+        border
       />
 
+      <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam iure doloremque, asperiores quasi, veniam fugiat, corrupti cupiditate similique ex obcaecati incidunt quos exercitationem architecto dolore rem vero at aspernatur!</p>
       <ImageViewer
         src={`${imageBasePath}/sim_metabolism_one_AP.png`}
         alt="Simulation metabolism AP"
-        className="mt-4"
+        border
       />
 
+      <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam iure doloremque, asperiores quasi, veniam fugiat, corrupti cupiditate similique ex obcaecati incidunt quos exercitationem architecto dolore rem vero at aspernatur!</p>
       <ImageViewer
         src={`${imageBasePath}/sim_metabolism_spikes.png`}
         alt="Simulation metabolism spikes"
-        className="mt-4"
+        border
       />
     </>
   );
