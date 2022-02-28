d5e80e18e4aa55478d868dc0a61b76ec22e944a4
Stefano Antonel <stefanoantonel@gmail.com>
2022-02-28 15:07:49 +0100

    Update simulation descriptions

--- src/components/Metabolism/DigitalReconstruction/SimulationResults/index.tsx
+++ src/components/Metabolism/DigitalReconstruction/SimulationResults/index.tsx
@@ -3,34 +3,34 @@ import React from 'react';
 import textContent from '@/text-content/metabolism/digital-reconstruction';
 import ImageViewer from '@/components/ImageViewer';
 import { basePath } from '@/config';
+import ImageAndDescriptionTemplate from '@/components/ImageAndDescriptionTemplate';
 
+const assetsBase = 'metabolism/digital-reconstruction';
 
 export default function SimulationResults() {
-  const imageBasePath = `${basePath}/assets/images/metabolism/digital-reconstruction/simulation`;
-
   return (
     <>
       <h4>{textContent.simulationResults}</h4>
 
-      <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam iure doloremque, asperiores quasi, veniam fugiat, corrupti cupiditate similique ex obcaecati incidunt quos exercitationem architecto dolore rem vero at aspernatur!</p>
-      <ImageViewer
-        src={`${imageBasePath}/figure_placeholder_sim_synaptic_metabolism.png`}
-        alt="Simulation synaptic metabolism"
-        border
+      <p className="mt-4"></p>
+      <ImageAndDescriptionTemplate
+        textKey="simSynapticMetabolism"
+        imagePath="simulation/figure_placeholder_sim_synaptic_metabolism.png"
+        assetsBase={assetsBase}
       />
 
-      <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam iure doloremque, asperiores quasi, veniam fugiat, corrupti cupiditate similique ex obcaecati incidunt quos exercitationem architecto dolore rem vero at aspernatur!</p>
-      <ImageViewer
-        src={`${imageBasePath}/sim_metabolism_one_AP.png`}
-        alt="Simulation metabolism AP"
-        border
+      <p className="mt-4"></p>
+      <ImageAndDescriptionTemplate
+        textKey="simMetabolismOneAP"
+        imagePath="simulation/sim_metabolism_one_AP.png"
+        assetsBase={assetsBase}
       />
 
-      <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam iure doloremque, asperiores quasi, veniam fugiat, corrupti cupiditate similique ex obcaecati incidunt quos exercitationem architecto dolore rem vero at aspernatur!</p>
-      <ImageViewer
-        src={`${imageBasePath}/sim_metabolism_spikes.png`}
-        alt="Simulation metabolism spikes"
-        border
+      <p className="mt-4"></p>
+      <ImageAndDescriptionTemplate
+        textKey="simMetabolismSpikes"
+        imagePath="simulation/sim_metabolism_spikes.png"
+        assetsBase={assetsBase}
       />
     </>
   );
