7f9d61d627b157d9eae931fac5800d5912740060
Stefano Antonel <stefanoantonel@gmail.com>
2022-03-16 09:15:29 +0100

    Update Prediction descriptions

--- src/text-content/metabolism/predictions.tsx
+++ src/text-content/metabolism/predictions.tsx
@@ -5,7 +5,15 @@ const textContent = {
   pageDescription: (<p>The simulation allows to analyse various high-level phenomena, some of those are shown in this section.</p>),
 
   oscillation: (<div>
-    <p>Glycolytic oscillations</p>
+    <p>
+      To our knowledge, there are only a few publications on the metabolic oscillations in the neurons (see the main text). We explored which parameter changes in the model lead to the transition from steady-state to metabolite concentrations oscillations. Biologically, simulated oscillations can potentially be interpreted as an adaptation to the low-ATP state.
+    </p>
+  </div>),
+
+  energy: (<div>
+    <p>
+      Na/K-ATPase action (Figure A) is one of the key energy consuming mechanisms which serves as a link between metabolism and neuronal firing in our model. For the Na/K-ATPase and many other enzymes, ATP/ADP ratio (Figure B) is one of the common factors affecting the rates of corresponding reaction. Over years, there was a lot of progress in defining the neuronal energy budget (see the paper's main text with references to the literature), and even specific numbers are still depend on necessary assumptions, synaptic and ionic gradients supporting processes (Figures C,D) are known as the key contributions to energy consumption. 
+    </p>
   </div>),
 };
 
