12854f37b8d26a6dcf316435bbf19c8614ba4f6a
Stefano Antonel <stefanoantonel@gmail.com>
2022-03-16 09:15:39 +0100

    Update Validation descriptions

--- src/text-content/metabolism/validations.tsx
+++ src/text-content/metabolism/validations.tsx
@@ -10,43 +10,46 @@ const textContent = {
   
   bold: (<div>
     <p>
-      BOLD - Blood-Oxygen-Level-Dependent signal
+      Blood-Oxygen-Level-Dependent signal.
     </p>
     <p>
-      BOLD response was calculated from the simulation and compared to literature data.
+      BOLD signal measurement is a method in functional magnetic resonance imaging to evaluate neuronal activity in different brain areas based on the oxygenation levels of haemoglobin. The background theory of this method is that increased neuronal activity is associated with higher oxygen input to neurons from the blood.
     </p>
   </div>),
 
   ogi: (<div>
     <p>
-      OGI - Oxygen Glucose Index
+      Oxygen Glucose Index.
     </p>
     <p>
-      OGI was calculated from the simulation and compared to literature data.
+      OGI is a measure of relative contributions of oxidative and non-oxidative pathways to glucose consumption. 
     </p>
   </div>),
 
   atp: (<div>
     <p>
-      ATP / Energy
+      Neuronal energy budget on different type of processes as compared to the literature (please see the paper's main text for details).
     </p>
   </div>),
 
   glucose: (<div>
     <p>
-      Glucose
+      Glucose is one of the key energy substrates.
+    </p>
+    <p>
+      The figure shows glucose concentration dynamics in response to neuronal stimulation.
     </p>
   </div>),
 
   lactate: (<div>
     <p>
-      Lactate
+      Lactate role in the brain is getting a lot of interest for both its energy substrate and regulatory roles (please see the paper's main text for details). The figure shows lactate concentration dynamics in response to neuronal stimulation.
     </p>
   </div>),
 
   nadh: (<div>
     <p>
-      NADH
+      NADH (reduced nicotinamide adenine dinucleotide) is an important reducing agent in biochemical reactions. Enzymes catalysing reactions involving NADH are attractive drug targets. 
     </p>
   </div>),
 };
