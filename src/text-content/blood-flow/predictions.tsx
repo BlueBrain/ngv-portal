5f2977e06cb5919850be1eb9c5c25af4362ad839
Stefano Antonel <stefanoantonel@gmail.com>
2022-03-30 15:40:19 +0200

    Add tentative text to pageDescription

--- src/text-content/blood-flow/predictions.tsx
+++ src/text-content/blood-flow/predictions.tsx
@@ -1,8 +1,8 @@
 
 const textContent = {
-  pageDescriptionMeta: '',
+  pageDescriptionMeta: 'The simulation allows us to get outputs such as variation over time following a stimulation of many parameters such as blood vessels diameter, etc. We observe changes in flow per each time step of the astrocyte\'s model',
 
-  pageDescription: (<p></p>),
+  pageDescription: (<p>The simulation allows us to get outputs such as variation over time following a stimulation of many parameters such as blood vessels diameter, etc. We observe changes in flow per each time step of the astrocyte's model</p>),
 };
 
 export default textContent;
--- src/text-content/blood-flow/validations.tsx
+++ src/text-content/blood-flow/validations.tsx
@@ -1,8 +1,8 @@
 
 const textContent = {
-  pageDescriptionMeta: '',
+  pageDescriptionMeta: 'Blood flow validation are of two types, standalone validation of spatial structure of the flow on the vasculature w.r.t biological measurements, and validation of flow response under time dependent vasodilatation from astrocytes activities.',
 
-  pageDescription: (<p></p>),
+  pageDescription: (<p>Blood flow validation are of two types, standalone validation of spatial structure of the flow on the vasculature w.r.t biological measurements, and validation of flow response under time dependent vasodilatation from astrocytes activities.</p>),
 };
 
 export default textContent;
