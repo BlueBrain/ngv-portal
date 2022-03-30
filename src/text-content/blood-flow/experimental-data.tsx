801c23c7f2dc397008f6272d8ca8ff9230c3336c
Stefano Antonel <stefanoantonel@gmail.com>
2022-03-30 12:56:19 +0200

    Add exp-data / Radii

--- src/text-content/blood-flow/experimental-data.tsx
+++ src/text-content/blood-flow/experimental-data.tsx
@@ -8,6 +8,18 @@ const textContent = {
     <p>A digital reconstruction of a rat cerebral vasculature dataset was produced by Reichold et al. (2009). Cylindrical blocks of the rat’s somatosensory cortex vasculature were scanned using synchrotron-based X-ray tomographic imaging at the TOMCAT beamline (Swiss Light Source). High energy beams (20KeV) irradiated the tissue with a resolution of (700 nm), resulting in grayscale image stacks, which were segmented into binary images and subsequently converted into midlines (skeleton) using custom software for artifact removal and skeletonization.</p>
     <p>The dataset consists of point samples linked together with edges. Each point is acquired with a diameter which represents the width of the cross section. Two consecutive points constitute a segment in the morphology, and consecutive segments form a morphological section.</p>
   </>),
+
+  radii: (<p>Astrocytes, play an important role in metabolism. Increased neuronal activity elevates calcium levels in astrocytes, which in turn releases vasoactive substances at the endfeet, causing changes in the diameter (vasodilation) of the associated blood vessel. Blood flow through the blood vessel (vasculature) network changes in response to altered diameters, meaning that delivery of nutrients (such as lactate, glucose, and oxygen) to support neuronal metabolism is in turn changed.</p>),
+
+  radiiLegend: (<>
+    <small>
+      <strong>Figure: </strong> 
+      Resmallresentation of realistic microvascular network samsmallle
+      (A) Synthesized astrocytic morsmallhologies
+      (B) One synthesized astrocytic morsmallhology with two endfeet
+      (C) NGV ensemble.
+    </small>
+  </>),
 };
 
 export default textContent;
