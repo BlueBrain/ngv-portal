8a4104d21a9cbc11a013b66c1e0c8142d7cbffab
Stefano Antonel <stefanoantonel@gmail.com>
2022-03-30 15:40:19 +0200

    Add tentative text to pageDescription

--- src/text-content/blood-flow/digital-reconstructions.tsx
+++ src/text-content/blood-flow/digital-reconstructions.tsx
@@ -1,9 +1,9 @@
 
 
 const textContent = {
-  pageDescriptionMeta: '',
+  pageDescriptionMeta: 'The endfeet stimulation first creates a vasodilation of blood vessels. This entails connecting the vasculature diameter to astrocytes via the endfeet (see anatomy section). The code stimulating one astrocyte in NEURON is thus linked with the code simulating the blood flow by using Python.',
 
-  pageDescription: (<p></p>),
+  pageDescription: (<p>The endfeet stimulation first creates a vasodilation of blood vessels. This entails connecting the vasculature diameter to astrocytes via the endfeet (see anatomy section). The code stimulating one astrocyte in NEURON is thus linked with the code simulating the blood flow by using Python. </p>),
 };
 
 export default textContent;
