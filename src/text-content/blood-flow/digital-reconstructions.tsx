362937f569e58824c7c4ffaa23ab903ea9a0d703
Stefano Antonel <stefanoantonel@gmail.com>
2022-05-03 08:48:45 +0200

    Update dig rec / landing page

--- src/text-content/blood-flow/digital-reconstructions.tsx
+++ src/text-content/blood-flow/digital-reconstructions.tsx
@@ -1,9 +1,13 @@
 
-
 const textContent = {
-  pageDescriptionMeta: 'The endfeet stimulation first creates a vasodilation of blood vessels. This entails connecting the vasculature diameter to astrocytes via the endfeet (see anatomy section). The code stimulating one astrocyte in NEURON is thus linked with the code simulating the blood flow by using Python.',
+  pageDescriptionMeta: 'Mathematical models of cerebral microcirculation are widely used in order to run hemodynamic simulations. These models imply to combine biophysical principles with data from medical imaging. For instance, data from synchrotron radiation based X-ray tomographic microscopy data of rat somatosensory cortex was used in Reichold (2009). These models have increasingly become an obvious field of research for cerebral circulation to get the whole NGV ensemble working together. This would enable a better understanding of the anatomical principles and geometric constraint, a better prediction of cerebral blood flow and, a better characterization of metabolites exchanges from and to tissues, and this could deepen our knowledge of the mechanisms involved in the blood flow control.',
 
-  pageDescription: (<p>The endfeet stimulation first creates a vasodilation of blood vessels. This entails connecting the vasculature diameter to astrocytes via the endfeet (see anatomy section). The code stimulating one astrocyte in NEURON is thus linked with the code simulating the blood flow by using Python. </p>),
+  pageDescription: (<p>
+    Mathematical models of cerebral microcirculation are widely used in order to run hemodynamic simulations. These models imply to combine biophysical principles with data from medical imaging. For instance, data from synchrotron radiation based X-ray tomographic microscopy data of rat somatosensory cortex was used in <a 
+    href="https://journals.sagepub.com/doi/10.1038/jcbfm.2009.58"
+    target="_blank"
+    rel="noopener noreferrer">Reichold (2009)</a>. These models have increasingly become an obvious field of research for cerebral circulation to get the whole NGV ensemble working together. This would enable a better understanding of the anatomical principles and geometric constraint, a better prediction of cerebral blood flow and, a better characterization of metabolites exchanges from and to tissues, and this could deepen our knowledge of the mechanisms involved in the blood flow control.
+  </p>),
 };
 
 export default textContent;
