97906e19a5287ccc8eac6bdfb409b35785c693ff
Stefano Antonel <stefanoantonel@gmail.com>
2023-08-18 10:56:56 +0200

    Add collab notebook

---
+++ src/components/Metabolism/DigitalReconstruction/CollabNotebook.tsx
@@ -0,0 +1,12 @@
+export default function CollabNotebook() {
+  return (
+    <>
+      <p>
+        You can run and modify the model using the following <a href="https://colab.research.google.com/drive/12EZSRjzq5eIaezpT41kv0e7LBMWFDZ_Y?usp=sharing " target="_blank" rel="noreferrer">Google Collab Notebook</a>.
+      </p>
+      <p>
+        You will need a Google account to use Google Collab. Then you need to copy the notebook to your Google Drive (click on File -&gt; Save a copy in Drive). Please follow the instructions in the Google Collab Notebook to run it. You can also download the notebook from Google Collab to run it locally, but it that case you need to have Julia and all packages used in the notebook. 
+      </p>
+    </>
+  );
+}
