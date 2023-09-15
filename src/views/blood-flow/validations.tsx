5c65ac6fd4987f0f6ffee8027b0fc7732661605f
Stefano Antonel <stefanoantonel@gmail.com>
2023-09-15 14:01:42 +0200

    Add blood flow validation

--- src/views/blood-flow/validations.tsx
+++ src/views/blood-flow/validations.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { Row, Col } from 'antd';
 import Image from 'next/image';
 
@@ -11,12 +11,18 @@ import { antBreakpoint, basePath } from '@/config';
 import textContent from '@/text-content/blood-flow/validations';
 import { Color } from '@/types';
 import { PageTitles } from '@/layouts/Navigation/constans';
+import ImageAndDescriptionTemplate from '@/components/ImageAndDescriptionTemplate';
 
 const primaryColor: Color = "red";
 
-const imgBase = `${basePath}/assets/images/validations`;
-
 const assetsBase = 'blood-flow/validations';
+
+const subtitleStyle: CSSProperties = {
+  fontSize: '1.4em',
+  marginTop: '35px',
+  marginBottom: '10px',
+  fontWeight: 'bold',
+}
 
 export default function BloodFlowValidationsView() {
   return (
@@ -56,16 +62,109 @@ export default function BloodFlowValidationsView() {
       </Filters>
 
       <DataContainer>
-        <DataSection title="Placeholder">
+        <DataSection title="Model validation with respect to literature data">
           <Row gutter={32}>
             <Col xs={24} md={24}>
+                {textContent.introductionTextSection1}
+            </Col>
+          </Row>
+
+          <p style={subtitleStyle}>▬▬ Blood flow and velocity values</p>
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="figure1Title"
+                imagePath="Figure_1.png"
+                assetsBase={assetsBase}
+                imgLegend={textContent.figure1ImgLegend}
+                columnFormat
+              />
+            </Col>
+          </Row>
+
+          <p style={subtitleStyle}>▬▬ Blood flow and velocity distribution</p>
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="figure2Title"
+                imagePath="Figure_2.png"
+                assetsBase={assetsBase}
+                imgLegend={textContent.figure2ImgLegend}
+                columnFormat
+              />
+            </Col>
+          </Row>
+
+          <p style={subtitleStyle}>▬▬ Blood flow and velocity distribution</p>
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="figure3Title"
+                imagePath="Figure_3.png"
+                assetsBase={assetsBase}
+                imgLegend={textContent.figure3ImgLegend}
+                columnFormat
+              />
             </Col>
           </Row>
         </DataSection>
 
-        <DataSection title="Placeholder 2" className="mt-4">
+        <DataSection title="In-depth analysis of flow values" className="mt-4">
           <Row gutter={32}>
             <Col xs={24} md={24}>
+              <div>{textContent.introductionTextSection2}</div>
+            </Col>
+          </Row>
+
+          <p style={subtitleStyle}>▬▬ Capillary diameter ranging from 1 to 3 μm</p>
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="figure4Title"
+                imagePath="Figure_4.png"
+                assetsBase={assetsBase}
+                imgLegend={textContent.figure4ImgLegend}
+                columnFormat
+              />
+            </Col>
+          </Row>
+
+          <p style={subtitleStyle}>▬▬ Capillary diameter ranging from 3 to 5 μm</p>
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="figure5Title"
+                imagePath="Figure_5.png"
+                assetsBase={assetsBase}
+                imgLegend={textContent.figure5ImgLegend}
+                columnFormat
+              />
+            </Col>
+          </Row>
+
+          <p style={subtitleStyle}>▬▬ Capillary diameter ranging from 5 to 7 μm</p>
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="figure6Title"
+                imagePath="Figure_6.png"
+                assetsBase={assetsBase}
+                imgLegend={textContent.figure5ImgLegend}
+                columnFormat
+              />
+            </Col>
+          </Row>
+        </DataSection>
+
+        <DataSection title="AstroVascPy (Open Source project)" className="mt-4">
+          <Row gutter={32}>
+            <Col xs={24} md={24}>
+              <ImageAndDescriptionTemplate
+                textKey="openSourcingDescription"
+                imagePath="AstroVascPy.jpg"
+                assetsBase={assetsBase}
+                columnFormat
+              />
             </Col>
           </Row>
         </DataSection>
